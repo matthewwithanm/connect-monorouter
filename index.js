90a5a0795e4f43f17ec605e12ab657b292454aad
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-09-30 16:54:29 -0400

    Pass entire URL to monorouter
    This way, we’ll have access to the domain info. And hey, as a bonus, no
    dependencies anymore!

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var urllite = require('urllite');
-
-
 var connectMiddleware = function(Router) {
   return function(req, res, next) {
-    // Get the part of the URL we care about.
-    // TODO: Allow this to be mounted at a different base and strip that from pathname
-    var parsed = urllite(req.url);
-    var url = parsed.pathname + parsed.search + parsed.hash;
+    var url = req.protocol + '://' + req.get('host') + req.originalUrl;
 
+    // TODO: Allow this to be mounted at a different base and strip that from pathname
     new Router().dispatch(url, {initialOnly: true}, function(err, rres) {
       if (err) {
         // The router doesn't want to handle it. That's okay, let something
